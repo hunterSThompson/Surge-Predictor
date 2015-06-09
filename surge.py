3a3fb695be650134eff69b217ac0dd68b7570486
hunterSThompson <hunt.b.graham@gamil.com>
2015-06-09 04:39:55 +0000

    Added locations config file.

--- surge.py
+++ surge.py
@@ -4,62 +4,77 @@ import requests
 import json
 import MySQLdb
 
-# Initiate cursor
-#cnx = MySQLdb.connect(host='', port=3306, passwd='bets123', user='bets', db='bets')
-cnx = MySQLdb.connect(host='', port=3306, passwd='gamera@1234',
-                                        user='bets', db='bets')
-cursor = cnx.cursor()
-
 start_long, start_lat = '42.340152', '-71.089389'
 end_long, end_lat = '42.318911', '-71.113336'
 
-# Formate URL with auth key and locations
+# Read in what locations to log surge for from locations.txt
+locations = [ ]
+for line in open('locations.txt', 'r'):
+	locations.append(tuple(line.split(',')))
+print 'Locations:\n' + str(locations)
+
+# Formate URL with auth key
 headers = { 'Authorization': 'Token 1tmfoN1a7nquX5cIV96_ur_1RioQrHqxuxWcKDNi' }
 base_url = 'https://api.uber.com/v1/estimates/price?start_latitude=%s&start_longitude=%s&end_latitude=%s&end_longitude=%s'
-url = base_url % (start_long, start_lat, end_long, end_lat)
 
-# Make request
-r = requests.get(url, headers=headers)
-#print r.text
+for (loc, lon, lat) in locations:
 
-# Parse JSON
-decoded = json.loads(r.text)
-black = decoded['prices'][0]['surge_multiplier']
-suv = decoded['prices'][1]['surge_multiplier']
-taxi = decoded['prices'][2]['surge_multiplier']
-uberX = decoded['prices'][3]['surge_multiplier']
-uberXL = decoded['prices'][4]['surge_multiplier']
-print black
-print suv 
-print taxi
-print uberX
-print uberXL
+	#import pdb; pdb.set_trace()
 
-data = (black, uberX, uberXL, suv, taxi, start_long, start_lat)
+	# Formate URL to enclude start lat/long
+	url = base_url % (lon, lat, end_long, end_lat)
 
-insert = '''
-INSERT INTO `bets`.`Surge`
-(
-`Time`,
-`UberBlack`,
-`UberX`,
-`UberXL`,
-`UberSUV`,
-`UberTaxi`,
-`Longitude`,
-`Latitude`
-)
-VALUES
-(
-NOW(),
-%s,
-%s,
-%s,
-%s,
-%s,
-%s,
-%s);
-'''
+	# Make request
+	r = requests.get(url, headers=headers)
 
-cursor.execute(insert, data)
-cnx.commit()
+	# Parse JSON
+	decoded = json.loads(r.text)
+	black = decoded['prices'][0]['surge_multiplier']
+	suv = decoded['prices'][1]['surge_multiplier']
+	taxi = decoded['prices'][2]['surge_multiplier']
+	uberX = decoded['prices'][3]['surge_multiplier']
+	uberXL = decoded['prices'][4]['surge_multiplier']
+	print '\n' + loc
+	print black
+	print suv 
+	print taxi
+	print uberX
+	print uberXL
+
+	# Insert into DB
+	#data = (black, uberX, uberXL, suv, taxi, start_long, start_lat)
+	data = (black, uberX, uberXL, suv, taxi, lon, lat, loc)
+	insert = '''
+	INSERT INTO `bets`.`Surge`
+	(
+	`Time`,
+	`UberBlack`,
+	`UberX`,
+	`UberXL`,
+	`UberSUV`,
+	`UberTaxi`,
+	`Longitude`,
+	`Latitude`,
+	`Location`
+	)
+	VALUES
+	(
+	NOW(),
+	%s,
+	%s,
+	%s,
+	%s,
+	%s,
+	%s,
+	%s,
+	%s);
+	'''
+	
+	#print insert % data
+	#pass
+	
+	# Initiate cursor
+	cnx = MySQLdb.connect(host='', port=3306, passwd='gamera@1234', user='bets', db='bets')
+	cursor = cnx.cursor()
+	cursor.execute(insert, data)
+	cnx.commit()
