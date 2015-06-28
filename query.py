0d64d5bf3ebd7496ae9ebad3a3a31c574fb1d968
huntersthompson <hunt.b.graham@gmail.com>
2015-06-28 17:21:42 -0400

    Added code to write bin files.

--- query.py
+++ query.py
@@ -7,6 +7,7 @@ import sklearn as sk
 import numpy
 import pygal
 import sys
+import os
 from datetime import date, timedelta
 
 day = 1
@@ -37,43 +38,58 @@ def createBins(lst, n):
 		bins.append(calcStats(chunk))
 	return bins
 
-def createChart(d, t, x, y):
+def calcDate(offset):
+	return date.today() - timedelta(days=offset)
+	
+
+def createChart(_d, t, x, y):
 	chart = pygal.Line()
 
-	d2 = date.today() - timedelta(days=d)
-	title = t + '_' + str(d2)
+	title = t + '_' + str(_d)
 	chart.title = title
 
 	chart.x_labels = x
 	chart.add(title, y)
 	chart.render_in_browser()
-	chart.render_to_file(title + '.html')
+	chart.render_to_file('Charts/' + title + '.html')
 
-# Read in what locations to log surge for from locations.txt
-locations = [ ]
+def writeBin(_loc, date_read, b):
+	filename = 'BinData/' + _loc +'_' + str(date_read) + '.bin'
+	str_data = '\n'.join(map(lambda t: ', '.join(str(i) for i in t), b))
+	open(filename, 'w').write(str_data)
+
+# Read in locations from locations.txt
+locations = []
 for line in open('locations.txt', 'r'):
         locations.append(tuple(line.split(',')))
 print 'Locations:\n' + str(locations)
 
+# Create a graph and .bin file each location
 for loc, lat, lon in locations:
 	try:
+		# Query surges for this location/date range
 		cnx = MySQLdb.connect(host='54.88.34.236', port=3306, passwd='gamera@1234', user='bets', db='bets')
 		cursor = cnx.cursor()
 		qstr = q4 % ((day*24)+4, loc)
 		cursor.execute(qstr)
+
 		surges,	times, dset = [], [], []
-	
 		for time, x, black in cursor.fetchall():
-			dset.append((time, black))
+			dset.append(black)
 			mins = (time.hour * 60) + time.minute
 			surges.append(black)
 			times.append(str(mins))
 	
-		#bins = createBins(dset, 15)
-		createChart(day, loc, times, surges)
-	except Exception, e:
-		print e
-#except Exception as e:
-    #exc_type, exc_obj, exc_tb = sys.exc_info()
-    #fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-    #print(exc_type, fname, exc_tb.tb_lineno)
+		#import pdb; pdb.set_trace()
+		reading_date = calcDate(day)
+
+		bins = createBins(dset, 15)
+		writeBin(loc, reading_date, bins)
+
+		createChart(reading_date, loc, times, surges)
+	#except Exception, e:
+		#print e
+	except Exception as e:
+    		exc_type, exc_obj, exc_tb = sys.exc_info()
+    		fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+    		print(exc_type, fname, exc_tb.tb_lineno, e)
